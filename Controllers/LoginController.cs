288eb447313cee8d12b83a163de2bfb064a1fe80
mahiuddin-dev <codermahiuddin@gmail.com>
2023-01-03 13:32:03 +0600

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using LoginApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public string Login(UserModel userModel)
+        {
+            return "Results: Username = " + userModel.Username + " Password = " + userModel.Password;
+        }
+
+    }
+}
